956f33c3fde4ab99f458082c3d43e1762942ea65
Jonathan Chuang <jochuang@umich.edu>
2020-05-02 23:36:54 -0400

    changed placement of darkmode button and time

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -1,14 +1,14 @@
 var options = {
   bottom: '32px', // default: '32px'
   right: 'unset', // default: '32px'
-  left: '32px', // default: 'unset'
-  time: '0.3s', // default: '0.3s'
+  left: '24px', // default: 'unset'
+  time: '0.5s', // default: '0.3s'
   mixColor: '#fff', // default: '#fff'
   backgroundColor: '#fff',  // default: '#fff'
   buttonColorDark: '#100f2c',  // default: '#100f2c'
   buttonColorLight: '#fff', // default: '#fff'
   saveInCookies: false, // default: true,
-  label: '🌓', // default: ''
+  label: '🌗', // default: ''🌓
   autoMatchOsTheme: true // default: true
 }
 
