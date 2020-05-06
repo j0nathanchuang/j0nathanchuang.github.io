06a700fa1b8c50f0bfaf6ceb38ceec58f79dcdab
Jonathan Chuang <jochuang@umich.edu>
2020-05-05 23:37:58 -0400

    Primary change: tabs to panels Fixed avatar orientation on mobile view shoutout Kevin comment on LinkedIn Attempting to add dark mode for mobile version, button is there, js missing

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,6 +45,22 @@ function openCity(evt, cityName) {
 
 (function($) {
 
+  //Create different panels
+  var acc = document.getElementsByClassName("accordion");
+  var i;
+
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.display === "block") {
+        panel.style.display = "none";
+      } else {
+        panel.style.display = "block";
+      }
+    });
+  }
+
 	var $window = $(window),
 		$body = $('body'),
 		$header = $('#header'),
