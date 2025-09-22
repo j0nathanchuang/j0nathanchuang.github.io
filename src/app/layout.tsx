f810e6aaf0647ca8a77437d8adf67cd9f7c59ddc
j0nathanchuang <jochuang@umich.edu>
2025-09-21 20:22:00 -0500

    Add favicon.ico to /public
    Fixes favicon not appearing in google search results

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -82,6 +82,15 @@ export const metadata: Metadata = {
     google:
       'google-site-verification=dxifpzV7nwCnk_wZVcCedBdwt2p6P7oxMG6i-W6WtpI',
   },
+  icons: {
+    icon: [
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ],
+    apple: [
+      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
+    ],
+  },
 };
 
 export default function RootLayout({
